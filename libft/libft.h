9160535a7933c5915014acc38e93e45b81451d30
Bongani MSIMANGA <bmsimang@e4r17p7.wethinkcode.co.za>
2018-06-05 17:00:11 +0200

    shiiit

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bmsimang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/01 09:56:22 by bmsimang          #+#    #+#             */
-/*   Updated: 2018/06/05 13:59:05 by bmsimang         ###   ########.fr       */
+/*   Updated: 2018/06/05 16:54:29 by bmsimang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void	*ft_mechr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
