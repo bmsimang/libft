e76fe1073858efcdacf317fe59365032c653d5a6
Bongani MSIMANGA <bmsimang@e4r17p7.wethinkcode.co.za>
2018-06-11 15:45:05 +0200

    part2

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmsimang <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/08 16:08:31 by bmsimang          #+#    #+#             */
+/*   Updated: 2018/06/11 15:38:13 by bmsimang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	has_whitespaces(char *str, int *i, size_t *j)
+{
+	int		IS_SPACE;
+	while (IS_SPACE(*(str + *i)))
+		(*i)++;
+	while (IS_SPACE(*(str + *j)))
+		(*j)--;
+	if (*i || *j < ft_strlen(str))
+		return (1);
+	return (0);
+}
+
+char		*ft_strtrim(char const *s)
+{
+	int		i;
+	size_t	j;
+	int		k;
+	char	*new_str;
+	size_t	new_size;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	k = 0;
+	j = ft_strlen(s) - 1;
+	if (!has_whitespaces((char *)s, &i, &j) || !ft_strlen(s))
+		return ((char *)s);
+	new_size = (i == (int)ft_strlen(s)) ? 0 : ft_strlen(s) - (size_t)i - \
+				(ft_strlen(s) - j);
+	new_str = ft_strnew(new_size + 1);
+	if (!new_str)
+		return (NULL);
+	while (i <= (int)j)
+		*(new_str + k++) = *(s + i++);
+	return (new_str);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bmsimang <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/01 09:56:22 by bmsimang          #+#    #+#             */
-/*   Updated: 2018/06/05 16:54:29 by bmsimang         ###   ########.fr       */
+/*   Updated: 2018/06/11 15:40:19 by bmsimang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,27 @@ int		ft_toupper(int c);
 char	*ft_strnew(size_t size);
 int		ft_islower(int c);
 int		ft_isupper(int c);
+
+char	*ft_itoa(int n);
+void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
+void	ft_putchar(char c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl(char const *s);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr(char const *s);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_strclr(char *s);
+void	ft_strdel(char **as);
+int		ft_strequ(char const *s1, char const *s2);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strjoin(char const *s1, char const *s2);
+char	 *ft_strmap(char const *s, char (*f)(char));
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	**ft_strsplit(char const *s, char c);
+char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strtrim(char const *s);
 
 #endif
